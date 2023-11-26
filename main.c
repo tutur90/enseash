ecc7aee16e3595520e198c5427b850856b88095b
Arthur Garon <arthurgaron@gmail.com>
2023-11-26 20:15:13 +0100

    test

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+#define BUFFER_SIZE 1024
+
 int main() {
-    printf("Bienvenue dans le Shell ENSEA.\n"
-           "Pour quitter, tapez 'exit'.\n"
-           "enseash %");
+    char buffer[BUFFER_SIZE]="Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %";
+    write(1, buffer, BUFFER_SIZE);
+    while(1) {
+        char buffer[BUFFER_SIZE];
+        int n = read(0, buffer, BUFFER_SIZE);
+        if (n == 0) {
+            break;
+        }
+        if (buffer =='exit') {
+            printf("Au revoir !\n");
+            break;
+        }
+        write(1, buffer, n);
+
+    }
     return 0;
 }
