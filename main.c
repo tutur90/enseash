bbaa50ba1928d10f1b7013f6e69f65a074bb94a1
Arthur Garon <arthurgaron@gmail.com>
2023-11-27 08:30:42 +0100

    rebase

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 
 int main() {
     char buffer[BUFFER_SIZE]="Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %";
-    write(1, buffer, BUFFER_SIZE);
-    while(1) {
-        char buffer[BUFFER_SIZE];
-        int n = read(0, buffer, BUFFER_SIZE);
-        if (n == 0) {
-            break;
-        }
-        if (buffer =='exit') {
-            printf("Au revoir !\n");
-            break;
-        }
-        write(1, buffer, n);
-
-    }
+    write(1, buffer, strlen(buffer));
+//    while(1) {
+//        char buffer[BUFFER_SIZE];
+//        int n = read(0, buffer, BUFFER_SIZE);
+//        if (n == 0) {
+//            break;
+//        }
+//        if (buffer =='exit') {
+//            printf("Au revoir !\n");
+//            break;
+//        }
+//        write(1, buffer, n);
+//
+//    }
     return 0;
 }
