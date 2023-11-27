67a4477a2775b536ef46a38831ea3a35fa422c35
tutur90 <arthurgaron@gmail.com>
2023-11-28 00:00:33 +0100

    Question 5

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
-
-
-
 int main() {
-
     char buffer[BUFFER_SIZE];
-    char *argv[10]; // Command-line arguments
-
+    char prompt[PROMPT_SIZE] = PROMPT;
+   
     write(1, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
-    write(1, PROMPT, strlen(PROMPT));
 
+    write(1, PROMPT, strlen(PROMPT));
     while(1) {
-        getCommand(buffer, argv); // Read and parse user input
+        int command_size = read(0, buffer, BUFFER_SIZE);
+        buffer[command_size - 1] = '\0';
+
         if (strcmp(buffer,"exit") == 0){
             write(1, EXIT_MESSAGE, strlen(EXIT_MESSAGE));
             break;
         }
-        exeCommand(buffer, argv); // Execute the command
+        exeCommand(buffer, NULL);
     }
     exit(EXIT_SUCCESS);
 }
