eaae6d5388761b9e3b3346679076c1135792d1c0
Arthur Garon <arthurgaron@gmail.com>
2023-11-27 09:23:30 +0100

    test

--- main.c
+++ main.c
@@ -3,12 +3,44 @@
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
-#define FILE_DESCRIPTION 0
+#define WELCOME_MESSAGE "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
+#define EXIT_MESSAGE "Bye bye ...\n"
+#define PROMPT "enseash %"
+
 
 
 int main() {
-    char buffer[BUFFER_SIZE]="Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %";
-    write(FILE_DESCRIPTION, buffer, strlen(buffer));
 
+    char buffer[BUFFER_SIZE];
+    write(1, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
+
+    while(1) {
+        write(1, PROMPT, strlen(PROMPT));
+        int command_size = read(0, buffer, BUFFER_SIZE);
+        buffer[command_size - 1] = '\0';
+
+        int response = exeCommand(buffer, command_size);
+        if (response == -1) {
+            write(1, "Commande introuvable.\n", strlen("Commande introuvable.\n"));
+        }
+
+//        if (strcmp(buffer, "exit") == 0) {
+//            write(1, EXIT_MESSAGE, strlen(EXIT_MESSAGE));
+//
+//            break;
+//        }
+    }
     return 0;
 }
+
+//int exeCommand(char *command, int command_size) {
+//    int pid = fork();
+//    if (pid == 0) {
+//        // Child process
+//        execlp(command, command, NULL);
+//    } else {
+//        // Parent process
+//        wait(NULL);
+//    }
+//    return 0;
+//}
