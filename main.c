43c0d0ad1282b23005aa55f862b09e31d34b0111
tutur90 <arthurgaron@gmail.com>
2023-11-27 11:09:05 +0100

    Question 2

--- main.c
+++ main.c
@@ -3,17 +3,47 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <wait.h>
+
 
 #define BUFFER_SIZE 1024
 #define WELCOME_MESSAGE "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
+#define EXIT_MESSAGE "Bye bye ...\n"
+#define PROMPT "enseash %"
 
-
+int exeCommand(char *command, ssize_t command_size);
 
 int main() {
 
     char buffer[BUFFER_SIZE];
     write(1, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
 
+    while(1) {
+        write(1, PROMPT, strlen(PROMPT));
+        int command_size = read(0, buffer, BUFFER_SIZE);
+        buffer[command_size - 1] = '\0';
 
+        int response = exeCommand(buffer, command_size);
+        if (response == -1) {
+            write(1, "Commande introuvable.\n", strlen("Commande introuvable.\n"));
+        }
+
+    }
+    return 0;
+}
+
+int exeCommand(char *command, ssize_t command_size) {
+    int pid = fork();
+    int status;
+
+    if (pid == 0) {
+        // Child process
+        execlp(command, command, NULL);
+        exit(EXIT_SUCCESS);
+
+    } else {
+        // Parent process
+        wait(NULL);
+    }
     return 0;
 }
